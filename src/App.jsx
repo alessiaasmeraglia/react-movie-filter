e8c18b768273931fabd2109a0084655c8d89f7a4
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-06 15:12:27 +0200

    Aggiunto array di oggetti con i film

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
+const moviesData = [
+  { id: 1, title: 'Inception', genre: 'Fantascienza' },
+  { id: 2, title: 'Il Padrino', genre: 'Thriller' },
+  { id: 3, title: 'Titanic', genre: 'Romantico' },
+  { id: 4, title: 'Batman', genre: 'Azione' },
+  { id: 5, title: 'Interstellar', genre: 'Fantascienza' },
+  { id: 6, title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 function App() {
   
