d5474074913bd5332c8ac5b5c47123bf6283fbc2
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-07 09:58:57 +0200

    Separato file js con array di oggetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import moviesData from "./data/filmList";
 
-const moviesData = [
-  { id: 1, title: 'Inception', genre: 'Fantascienza' },
-  { id: 2, title: 'Il Padrino', genre: 'Thriller' },
-  { id: 3, title: 'Titanic', genre: 'Romantico' },
-  { id: 4, title: 'Batman', genre: 'Azione' },
-  { id: 5, title: 'Interstellar', genre: 'Fantascienza' },
-  { id: 6, title: 'Pulp Fiction', genre: 'Thriller' },
-]
 
 function App() {
   //stato base
